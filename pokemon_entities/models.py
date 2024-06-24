7d2aada9f2c20fe94f07a8027e7b307da95e112b
Yuriy Mikhailov <apofiz2036@bk.ru>
2024-06-24 07:11:59 +0500

    add specs to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -12,7 +12,15 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     title = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+
     lat = models.FloatField()
     lan = models.FloatField()
-    appeared_at = models.DateTimeField(default=timezone.now)
-    disappeared_at = models.DateTimeField(default=timezone.now)
+
+    appeared_at = models.DateTimeField(default=timezone.now, verbose_name='Время появления')
+    disappeared_at = models.DateTimeField(default=timezone.now, verbose_name="Время исчезновения")
+
+    level = models.IntegerField(default=1, verbose_name="Уровень")
+    health = models.IntegerField(default=1, verbose_name="Здоровье")
+    strength = models.IntegerField(default=1, verbose_name="Атака")
+    defence = models.IntegerField(default=1, verbose_name="Защита")
+    stamina = models.IntegerField(default=1, verbose_name="Выносливость")
